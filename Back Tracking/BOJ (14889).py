8d815f2ece67d9623f066aad5ba5d65a540bef2a
이성준 <75744297+castlejun-2@users.noreply.github.com>
2021-10-21 18:54:22 +0900

    Update BOJ (14889).py

--- Back Tracking/BOJ (14889).py
+++ Back Tracking/BOJ (14889).py
@@ -6,7 +6,7 @@ members = [i for i in range(T)]         #멤버의 이름
 team=list(combinations(members, T//2))  #가능한 팀의 조합 -> ex) 4C2, 6C2
 score_gap=[]
 for i in range(len(team)//2):
-    select_team = team[i]   
+    select_team = team[i]            #가능한 팀 조합에서 팀 하나 선택, 이 때 앞의 절반 i 팀과 -i-1 팀이 대항된다
     start=0                          #start team 능력치
     for j in range(T//2):
         member=select_team[j]        #멤버 선택

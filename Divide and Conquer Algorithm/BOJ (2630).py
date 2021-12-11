c48b4b1e9c99c3d8b1533b7a0b94f04a86a289b0
이성준 <75744297+castlejun-2@users.noreply.github.com>
2021-12-11 20:41:10 +0900

    BOJ Algorithm

--- Divide and Conquer Algorithm/BOJ (2630).py
+++ Divide and Conquer Algorithm/BOJ (2630).py
@@ -1,2 +1,26 @@
-N = int(input())
-m_list=[list(map(int,input().split())) for _ in range(N)]
+white = 0
+blue = 0
+
+def divide(x,y,N):
+    global white
+    global blue
+    c = m_list[x][y]
+    for i in range(x,x+N):
+        for j in range(y,y+N):
+            if c!=m_list[i][j]:             #범위 탐색중 다른 숫자가 있다면 범위를 2로 다시 분할
+                divide(x,y,N//2)  
+                divide(x+N//2,y,N//2)
+                divide(x,y+N//2,N//2)
+                divide(x+N//2,y+N//2,N//2)
+                return    
+    if c:                                   #탐색이 끝난 후 해당 컬러에 맞는 색종이의 색 +1
+        white+=1;
+    else:
+        blue+=1
+        
+if __name__=="__main__":
+    N = int(input())
+    m_list=[list(map(int,input().split())) for _ in range(N)]
+    divide(0,0,N)
+    print(blue)
+    print(white)

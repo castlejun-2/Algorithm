4aadafd6203e360ea037ea183713c0ad562ddf7f
이성준 <75744297+castlejun-2@users.noreply.github.com>
2021-12-30 22:46:48 +0900

    BOJ Algorithm

--- Greedy/BOJ (1439).py
+++ Greedy/BOJ (1439).py
@@ -1,4 +1,6 @@
-s_list=list(map(int,input()));  
+from sys import stdin
+
+s_list=list(map(int,stdin.readline().strip()));  #입력속도 향상을 위해 readline() 사용
 cnt=0
 for i in range(len(s_list)-1):    #000111000 은 010으로 표현이 가능, 00110011 은 0101로 표현이 가능하다.
     if s_list[i] != s_list[i+1]:  #0->1 혹은 1->0 이 되는 시점에 cnt++
@@ -7,7 +9,7 @@ print((cnt+1)//2)                 #010 이면 ->1 이 나오는데 이는 1+1//2
 
 --------------------
 
-s_list=list(map(int,input()));
+s_list=list(map(int,stdin.readline().strip()));
 k=s_list[0]
 cnt_0=0;
 cnt_1=0;

8a46eac4e03c3252b7dc7bf3185ee8932815fbcc
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-03-03 18:04:31 +0900

    BOJ Algorithm

--- Divide and Conquer Algorithm/BOJ (2263).py
+++ Divide and Conquer Algorithm/BOJ (2263).py
@@ -3,7 +3,13 @@ from sys import stdin
 def preorder(in_start,in_end,post_start,post_end):
     if(in_start>in_end) or (post_start>post_end): return
 
-    #left와 right 구하는 방법 고민
+    p_index=post_list[post_end]
+    print(p_index, end="")
+    
+    left=index[p_index]-in_start
+    right=in_end-index[p_index]
+    
+    #재귀를 어떻게 돌릴것인가!
 
 N=int(stdin.readline())
 in_list=list(map(int,stdin.readline().split()))

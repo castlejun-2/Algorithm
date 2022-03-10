031861a1f7098bf5c11d10741003926fdea7cc4c
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-03-10 16:22:45 +0900

    BOJ Algorithm

--- Data structure/BOJ (9375).py
+++ Data structure/BOJ (9375).py
@@ -2,13 +2,19 @@ from sys import stdin
 
 N=int(stdin.readline())
 
-for _ in range(N):
-    T=int(stdin.readline())
-    costume_list={}
-    for i in range(T):
-        costume,costume_type=map(str,stdin.readline().split())
-        if costume_type in costume_list:
-            costume_list[costume_type].append(costume)
-        else:
-            costume_list[costume_type] = [costume]
-     #저장한 딕셔너리 값들을 어떻게 끌어 사용할지 고민 or 값을 일일이 저장하지 않고 사용 할 수 있는 방법 고민
+if N==0:
+    print(0)
+else:
+    for _ in range(N):
+        T=int(stdin.readline())
+        costume_list={}
+        cnt_sum=1
+        for i in range(T):  #각 의상 종류에 맞는 의상을 삽입
+            costume,costume_type=map(str,stdin.readline().split())
+            if costume_type in costume_list:
+                costume_list[costume_type].append(costume)
+            else:
+                costume_list[costume_type] = [costume]
+        for c in costume_list:  #의상 종류에 n개의 의상이 있다면 n개중 1개의 의상을 고르는 갯수와 아무것도 고르지 않을 수 있으므로 n+1을 곱해준다.
+            cnt_sum*=len(costume_list[c])+1
+        print(cnt_sum-1)    #단 모든 의상을 입지 않는 경우는 빼주어야 하므로 -1을 해준다.

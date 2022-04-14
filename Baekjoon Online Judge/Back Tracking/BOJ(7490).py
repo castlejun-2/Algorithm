712602bc471750497934ef62a11182ad44eaa9aa
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-04-14 19:50:03 +0900

    BOJ Algorithm

--- Baekjoon Online Judge/Back Tracking/BOJ(7490).py
+++ Baekjoon Online Judge/Back Tracking/BOJ(7490).py
@@ -2,13 +2,12 @@ from sys import stdin
 
 T=int(stdin.readline().strip())
 
-def check(arr):
+def check(arr): #N만큼의 길이에 도달하면, eval 함수를 통해 해당 문자열의 결과가 0인지 확인한다.
     temp_arr=(''.join(map(str,arr.split(' '))))
-    if eval(temp_arr)==0:
+    if eval(temp_arr)==0:   # *eval*: 문자열인 계산식을 계산해주는 함수
         return True
     else:
         return False
-    
         
 def space_dfs(arr,s):
     arr+=(" "+str(s+1))
@@ -41,9 +40,10 @@ def sum_dfs(arr,s):
         mis_dfs(arr,s+1)
     
 for i in range(T):
-    N=int(stdin.readline().strip())
+    N=int(stdin.readline().strip()) #N이 3<=N<=9 이므로 완전탐색으로 풀어도 시간초과가 나지 않는다.
     
-    space_dfs("1",1)
+    #ASCII 코드 순서로 완전탐색을 한다
+    space_dfs("1",1)    
     sum_dfs("1",1)
     mis_dfs("1",1)
 

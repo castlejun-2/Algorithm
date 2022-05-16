1a480b7fa9ae8c32f5cd611047c916b0497acde2
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-05-17 00:27:10 +0900

    최대값들을 저장해 나아가 해결하는 문제풀이

--- 이코테/Dynamic Programming/정수 삼각형.py
+++ 이코테/Dynamic Programming/정수 삼각형.py
@@ -4,8 +4,7 @@ dp=[[0]*n for _ in range(n)]
 dp[0][0]=int(input().strip())
 for i in range(1,n):
     temp_list=list(map(int,input().strip().split()))
-    dp[i][0]=dp[i-1][0]+temp_list[0]
-    dp[i][i]=dp[i-1][i]+temp_list[-1]
-    for j in range(1,i):
-        dp[i][j]=max(dp[i-1][j],dp[i-1][j-1])+temp_list[j]
-print(max(dp[-1]))
+    dp[i][0]=dp[i-1][0]+temp_list[0]    #삼각형의 가장 앞은 그 위의 삼각형 가장 앞의 값만 이어 받을 수 있다.
+    for j in range(1,i+1):
+        dp[i][j]=max(dp[i-1][j],dp[i-1][j-1])+temp_list[j]  #그 외의 값들은 대각선의 값을 참고하며, 마지막 값은 그 위의 값이 0이므로 그대로 참고한다.
+print(max(dp[-1]))  #마지막 삼각형의 값들중 최대값을 출력한다.

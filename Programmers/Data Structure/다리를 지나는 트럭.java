2d830d2e4701380ac8137866a6c6a8a5d74892da
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-10-08 04:10:45 +0900

    큐의 값과, 크기를 비교하며 트럭을 올린 문제풀이

--- Programmers/Data Structure/다리를 지나는 트럭.java
+++ Programmers/Data Structure/다리를 지나는 트럭.java
@@ -8,12 +8,7 @@ class Solution {
 
         for (Integer tq : truck_weights) {
             while (true) {
-                if (q.isEmpty()) {  //다리가 비어있다면, 트럭을 올린다.
-                    q.add(tq);
-                    tmp += tq;
-                    answer++;
-                    break;
-                } else if (q.size()==bridge_length){  //다리위가 꽉 차면, 가장 앞의 트럭이 나간다.
+                if (q.size()==bridge_length){  //다리위가 꽉 차면, 가장 앞의 트럭이 나간다.
                     tmp -= q.poll();
                 } else {
                     if (tmp + tq <= weight) { //트럭이 올라갈 수 있으면 다리에 올라간다.

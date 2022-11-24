1e25aabb9ab86b3761a2debf4c676ba2d7946f3c
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-24 21:58:28 +0900

    fix: 행과 열을 먼저 update 하도록 수정

--- Programmers/Implementation/삼각 달팽이.py
+++ Programmers/Implementation/삼각 달팽이.py
@@ -1,21 +1,23 @@
 def solution(n):
     answer = [[0]*n for _ in range(n)]
     count = 1
-    col = 0; row = 0
-    for i in range(n-1,-1,-3):
+    col = 0; row = -1
+    for i in range(n,-1,-3):
         for j in range(i):
+            row+=1;     #행과 열의 값을 먼저 반영해주어야 한다.
             answer[row][col]=count
-            row+=1; count+=1;
+            count+=1;
         for j in range(i-1):
+            col+=1;
             answer[row][col]=count
-            col+=1; count+=1;
+            count+=1;
         for j in range(i-2):
+            col-=1; row-=1;
             answer[row][col]=count
-            col-=1; row-=1; count+=1
-    
+            count+=1
     ans = []
     
     for i in range(1,n+1):
-        for j in range(i):
+        for j in range(1,i+1):
             ans.append(answer[i-1][j-1])
     return ans

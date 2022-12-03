414670ee0efa194432fcb4810c53a188bdd584b5
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-04 01:26:58 +0900

    플로이드-워셜을 통해 모든 간선까지의 최단거리의 최솟값을 갖는 노드를 찾은 문제풀이

--- Baekjoon Online Judge/Graph Theory/BOJ (1389).py
+++ Baekjoon Online Judge/Graph Theory/BOJ (1389).py
@@ -1,7 +1,23 @@
 N,M=map(int,input().split())
-graph=[[0]*N for _ in range(N)]
+graph=[[float('inf')]*N for _ in range(N)]
+answer=[0]*N
 
 for i in range(M):
     A,B=map(int,input().split())
     graph[A-1][B-1]=1
-    grpah[B-1][A-1]=1
+    graph[B-1][A-1]=1
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if i==j:
+                continue
+            graph[i][j]=min(graph[i][j],graph[i][k]+graph[k][j])    #연결되어 있는 친구의 최솟값 탐색
+
+for i in range(N):
+    for j in range(N):
+        if i==j:
+            continue
+        answer[i]+=graph[i][j]      #자기 자신을 제외한 케빈 베이컨 법칙의 합 계산
+
+print(answer.index(min(answer))+1)  #최소값을 갖는 index 중 가장 앞의 값 반환
